ee6d7c94bae6f76c085713bec349d9b5d4f0550d
KuhleB <phiwokuhle.bongco@gmail.com>
2023-07-14 10:04:42 -0700

    101-quote commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-  * main - Print Message
-  * Return 1 (success)
+  * main - Print a quote
+  * Return: 1
   */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-	, stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	, 59);
 	return (1);
 }
