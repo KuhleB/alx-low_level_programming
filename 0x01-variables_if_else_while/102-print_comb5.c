05ebe1100948758d31c2ec77224b6c3dcdfa4e9b
KuhleB <phiwokuhle.bongco@gmail.com>
2023-07-16 10:38:56 -0700

    100-print_comb3 commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 99; j++)
+	for ((i = )0; i <= 99; j++)
 	{
-		for (j = 0; j <= 99; j++)
+		for ((j = 0); j <= 99; j++)
 		{
 			if (i < j && i != j)
 			{
