459cdb3f49a752baabb6cec05acbba7ab6787cc4
KuhleB <phiwokuhle.bongco@gmail.com>
2023-07-17 13:47:16 -0700

    100-print_comb3 commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
+
 /**
-  * main - Print message
-  *
-  * Description: Print possible combinations of two digit numbers
-  * Return: 0 on success
-  */
+ * main - prints numbers from 1 to 89, except multiples of 10 or 11
+ * Return: 0 on success
+ */
 int main(void)
 {
-    int num1, num2;
+    int num1;
 
-    for (num1 = 0; num1 <= 98; num1++)
+    for (num1 = 0; num1 < 90; num1++)
     {
-	    for (num2 = num1 + 1; num2 <= 99; num2++)
+	    if (num1 % 10 != 0 && num1 % 11 != 0)
 	    {
-		   if (num1 / 10)
-		   putchar((num1 / 10) + '0');
-		   }
-		   putchar((num2 % 10) + '0');
-
-		    if (num1 == 98 && num2 == 99)
-			    continue;
-
-		    putchar(',');
-		    putchar(' ');
+		    if (num1 / 10)
+		    {
+			    putchar(num1 / 10 + '0');
+		    }
+		    putchar(num1 % 10 + '0');
+		    if (num1 != 89)
+		    {
+			    putchar(',');
+			    putchar(' ');
+		    }
 	    }
-    putchar('\n');
-    return (0);
+	    }
+	    putchar('\n');
+	    return (0);
 }
+
