f313bc180a0ebfd360689a8bb978970c720e257a
KuhleB <phiwokuhle.bongco@gmail.com>
2023-07-17 13:08:05 -0700

    100-print_comb3 commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,11 +13,10 @@ int main(void)
     {
 	    for (num2 = num1 + 1; num2 <= 99; num2++)
 	    {
-		    putchar((num1 / 10) + '0');
-		    putchar((num1 % 10) + '0');
-		    putchar(' ');
-		    putchar((num2 / 10) + '0');
-		    putchar((num2 % 10) + '0');
+		   if (num1 / 10)
+		   putchar((num1 / 10) + '0');
+		   }
+		   putchar((num2 % 10) + '0');
 
 		    if (num1 == 98 && num2 == 99)
 			    continue;
@@ -25,7 +24,6 @@ int main(void)
 		    putchar(',');
 		    putchar(' ');
 	    }
-    }
     putchar('\n');
     return (0);
 }
