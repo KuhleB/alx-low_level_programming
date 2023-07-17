9f410668f9a27c8d1f43699b44f495c4b50040cb
KuhleB <phiwokuhle.bongco@gmail.com>
2023-07-17 14:34:18 -0700

    100-print_comb3 commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 
 /**
- * main - prints numbers from 1 to 89, except multiples of 10 or 11
+ * main - prints combination of  numbers
+ *
  * Return: 0 on success
  */
 int main(void)
 {
-	int num1;
+	int num1, num2;
 
-	for (num1 = 0; num1 < 90; num1++)
+	for (num1 = 48; num1 < 58; num1++)
 	{
-		if (num1 % 10 != 0 && num1 % 11 != 0)
+	num2 = 49 + (num1 - 48);
+	for (; num2 < 58 ; num2++)
+	{
+		putchar(num1);
+		putchar(num2);
+
+	if (num1 == 56 && num2 == 57)
+	{
+		continue;
 	}
-	if (num1 / 10)
-	{
-		putchar(num1 / 10 + '0');
-	}
-	putchar(num1 % 10 + '0');
-	if (num1 != 89)
-	{
 		putchar(',');
 		putchar(' ');
+	}
 	}
 	putchar('\n');
 	return (0);
