8ea58fa0ace96c7d81915145a069ac31b58b694f
KuhleB <phiwokuhle.bongco@gmail.com>
2023-08-03 03:24:49 -0700

    task commit 3 - factorial

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string input
+ * Return: 0 on success
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
+}
